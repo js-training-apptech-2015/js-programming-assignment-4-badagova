06dd464e6debc58bce8a062f36272bf15130c011
badagova <jenny_bad@mail.ru>
2015-12-08 21:08:14 +0500

    one method of loading all file types

--- JS-4-2.js
+++ JS-4-2.js
@@ -1,46 +1,18 @@
 function define(dependencies, callback){
-	var files = [];
+	var files = [],
 		promises = [];
 	for(var i = 0; i< dependencies.length; i++){
-		if ( dependencies[i].match(/\.png|\.jpg|\.gif|\.jpeg/) !== null){
-			files[i] = document.createElement("img");
-			files[i].setAttribute("src", dependencies[i]);
-			promises[i] = new Promise (function(resolve, reject) {
-				files[i].onload = function(){
-					resolve(this);
-				};
-				files[i].onerror = function(){
-					reject('error');
-				};
-			});
-			document.head.appendChild(files[i]);
-		} else if (dependencies[i].match(/\.txt/) !== null){
-			files[i] = new XMLHttpRequest();
-			files[i].open("GET", dependencies[i]);
-			promises[i] = new Promise (function(resolve, reject) {
-				files[i].onload = function(){
-					resolve(this.response);
-				};
-				files[i].onerror = function(){
-					reject('error');
-				};
-			});
-			files[i].send(null);
-		} else if (dependencies[i].match(/\.js/) !== null){
-			files[i] = document.createElement("script");
-			files[i].src = dependencies[i];		
-			promises[i] = new Promise (function(resolve, reject) {
-				files[i].onload = function(){
-					resolve(this);
-				};
-				files[i].onerror = function(){
-					reject('error');
-				};
-			});
-			document.head.appendChild(files[i]);
-		} else {
-			alert("Unknown type of file!");
-		}
+		files[i] = new XMLHttpRequest();
+		files[i].open("GET", dependencies[i]);
+		promises[i] = new Promise (function(resolve, reject) {
+			files[i].onload = function(){
+				resolve(this.response);
+			};
+			files[i].onerror = function(){
+				reject('error');
+			};
+		});
+		files[i].send(null);
 	}
 	Promise.all(promises).then(function(args){
 		callback.apply(callback,args);
